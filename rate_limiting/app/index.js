786c7ec2778f553b84f94296042a51f57703c952
marian1908 <marianaschulc@gmail.com>
2024-05-05 15:45:25 -0300

    rate_limiting fix

--- rate_limiting/app/index.js
+++ rate_limiting/app/index.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 
 const rateLimit = require('express-rate-limit');
 const StatsD = require('hot-shots');
+const middleware = require('./middleware/middleware');
 
 const stats = new StatsD(
     {
@@ -28,73 +29,73 @@ app.get('/ping', (req, res) => {
     const endpoint_start = Date.now();
     res.status(200).send('pong')
     const endpoint_time = Date.now() - endpoint_start;
-    stats.gauge('endpoint_time', endpoint_time);
+    stats.timing('endpoint_time_stats', endpoint_time);
 });
 
-app.get('/dictionary', async (req, res) => {
+app.get('/dictionary', async (req, res, next) => {
     const endpoint_start = Date.now();
+    const api_start = Date.now();
     try {
-        const api_start = Date.now();
         const response = await axios({
             method: 'get',
             url: `https://api.dictionaryapi.dev/api/v2/entries/en_US/${req.query.word}`
         })
+        const api_time = Date.now() - api_start;
+        stats.timing('external_api_time', api_time);
         const data = [
             {
                 phonetics: response.data[0].phonetics,
                 meanings: response.data[0].meanings
             }
         ]
-        console.log(response.data[0].phonetics)
-        const api_time = Date.now() - api_start;
-        stats.gauge('external_api_time', api_time);
         res.status(200).send(data)
     }
     catch (error) {
-        res.status(500).send('An error occurred')
-        console.log(error)
+        const api_time = Date.now() - api_start;
+        stats.timing('external_api_time_stats', api_time);
+        next(error)
     }
     const endpoint_time = Date.now() - endpoint_start;
-    stats.gauge('endpoint_time', endpoint_time);
+    stats.timing('endpoint_time_stats', endpoint_time);
 });
 
-app.get('/spaceflight_news', async (req, res) => {
+app.get('/spaceflight_news', async (req, res, next) => {
     const endpoint_start = Date.now();
+    const api_start = Date.now();
     try {
-        const api_start = Date.now();
         const response = await axios({
             method: 'get',
             url: 'https://api.spaceflightnewsapi.net/v4/articles'
         })
+        const api_time = Date.now() - api_start;
+        stats.timing('external_api_time_stats', api_time);
         const data = response.data.results.slice(0, 5).map((article) => {
             return (
                 article.title
             )
         })
-        const api_time = Date.now() - api_start;
-        stats.gauge('external_api_time', api_time);
         res.status(200).send(data)
 
     }
     catch (error) {
-        res.status(500).send('An error occurred')
-        console.log(error)
+        const api_time = Date.now() - api_start;
+        stats.timing('external_api_time_stats', api_time);
+        next(error)
     }
     const endpoint_time = Date.now() - endpoint_start;
-    stats.gauge('endpoint_time', endpoint_time);
+    stats.timing('endpoint_time_stats', endpoint_time);
 });
 
-app.get('/quote', async (req, res) => {
+app.get('/quote', async (req, res, next) => {
     const endpoint_start = Date.now();
+    const api_start = Date.now();
     try {
-        const api_start = Date.now();
         const response = await axios({
             method: 'get',
             url: 'https://api.quotable.io/random'
         })
         const api_time = Date.now() - api_start;
-        stats.gauge('external_api_time', api_time);
-
+        stats.timing('external_api_time_stats', api_time);
         const data = [
             {
                 content: response.data.content,
@@ -104,12 +105,15 @@ app.get('/quote', async (req, res) => {
         res.status(200).send(data)
     }
     catch (error) {
-        res.status(500).send('An error occurred')
-        console.log(error)
+        const api_time = Date.now() - api_start;
+        stats.timing('external_api_time_stats', api_time);
+        next(error)
     }
     const endpoint_time = Date.now() - endpoint_start;
-    stats.gauge('endpoint_time', endpoint_time);
+    stats.timing('endpoint_time_stats', endpoint_time);
 });
+
+app.use(middleware.errorHandler);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
